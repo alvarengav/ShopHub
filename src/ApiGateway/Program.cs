1aac410ebc148082da8d808a9ff420d97d1c2685
Luis Alvarenga <l.alvarengav@gmail.com>
2024-07-26 13:20:29 -0600

    feat: configure Ocelot for correct service routing
    - Updated ocelot.json to correctly route requests to OrderService and UserService.
    - Verified service communication and Swagger UI functionality.

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,8 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,8 +19,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerForOcelotUI(opt =>
+        {
+            opt.PathToSwaggerGenerator = "/swagger/docs";
+        })
+        .UseOcelot()
+        .Wait();
 }
+
+app.UseOcelot().Wait();
 
 app.UseHttpsRedirection();
 
---
+++ src/OrderService/OrderService.API/Controllers/OrdersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrdersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetOrders()
+    {
+        return Ok(new { Meessage = "Orders endpoint" });
+    }
+}
